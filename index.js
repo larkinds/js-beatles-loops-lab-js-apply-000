a576c59c7ec3d1065f82fc17db25866ddea71c56
larkindsmith <larkindsmith@gmail.com>
2018-12-21 18:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const theBeatlesPlay = (arrayM, arrayI) => {
   let varE= []
   let emptyString = ''
   for (let i = 0; i < arrayM.length; i++) {
-    emptyString = (arrayM[i] + ' plays ' + arrayI[i] + '.')
+    emptyString = ("arrayM[i] plays arrayI[i].")
     varE += emptyString
   }
   return VarE
